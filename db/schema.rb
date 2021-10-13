b4abaf0e9144c7d3b234d039ac82b587284647b2
ryota1119 <ibaryo.ngok.sxs@gmail.com>
2021-10-13 22:49:52 +0900

    markup index page

--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2021_10_12_120214) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "name", null: false
+    t.string "name", default: "", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
