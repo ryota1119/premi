ada831996e9b81442c3b5d94a2409a189fefa738
ryota1119 <ibaryo.ngok.sxs@gmail.com>
2021-10-23 21:29:03 +0900

    Setting validation

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,8 +1,10 @@
 class Company < ApplicationRecord
+  VALID_PHONE_REGEX = /\A(((0(\d{1}[-(]?\d{4}|\d{2}[-(]?\d{3}|\d{3}[-(]?\d{2}|\d{4}[-(]?\d{1}|[5789]0[-(]?\d{4})[-)]?)|\d{1,4}\-?)\d{4}|0120[-(]?\d{3}[-)]?\d{3})\z/
+  VALID_POSTNUM_REGEX = /\A\d{3}[-]\d{4}$|^\d{3}[-]\d{2}$|^\d{3}$|^\d{5}$|^\d{7}\z/
   belongs_to :user
   validates :company_name, presence: true
-  validates :tel, presence: true
-  validates :fax, presence: true
-  validates :postal_code, presence: true
+  validates :tel, presence: true, format: { with: VALID_PHONE_REGEX }
+  validates :fax, presence: true, format: { with: VALID_PHONE_REGEX }
+  validates :postal_code, presence: true, format: { with: VALID_POSTNUM_REGEX }
   validates :address, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :name, presence: true, length: { maximum: 50 }
+  validates :email, uniqueness:true, length:{ maximum: 255 },
+            format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :password, length: { minimum: 6 },
+            format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
 end
