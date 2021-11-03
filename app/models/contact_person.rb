4d979b413ab60134e559fc765ecc5e1569026932
ryota1119 <ibaryo.ngok.sxs@gmail.com>
2021-11-03 22:24:31 +0900

    Create estimates index page

--- app/models/contact_person.rb
+++ app/models/contact_person.rb
@@ -1,4 +1,5 @@
 class ContactPerson < ApplicationRecord
+  has_one :estimate
   belongs_to :customer
   validates :name, presence: true
 end
--- app/models/estimate.rb
+++ app/models/estimate.rb
@@ -2,4 +2,5 @@ class Estimate < ApplicationRecord
   has_many :estimate_details, dependent: :destroy
   belongs_to :customer
   belongs_to :user
+  belongs_to :contact_person
 end
