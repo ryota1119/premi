e4d6ec1fc847b2b97f38e60682892fd5b02fd983
ryota1119 <ibaryo.ngok.sxs@gmail.com>
2021-11-07 21:23:05 +0900

    Edit model test

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -20,41 +20,45 @@ RSpec.describe Company, type: :model do
       expect(company.errors[:tel]).to include("を入力してください")
     end
 
-    it "telのフォーマットが正しくない場合、登録に失敗すること" do
-      company = build(:company, tel: "1000000000")
-      company.valid?
-      expect(company.errors[:tel]).to include("は不正な値です")
-    end
-
+    
     it "faxがない場合は登録に失敗すること" do
       company = build(:company, fax: nil)
       company.valid?
       expect(company.errors[:fax]).to include("を入力してください")
     end
-
-    it "faxのフォーマットが正しくない場合、登録に失敗すること" do
-      company = build(:company, fax: "1000000000")
-      company.valid?
-      expect(company.errors[:fax]).to include("は不正な値です")
-    end
-
+    
+    
     it "postal_codeがない場合は登録に失敗すること" do
       company = build(:company, postal_code: nil)
       company.valid?
       expect(company.errors[:postal_code]).to include("を入力してください")
     end
-
-    it "postal_codeのフォーマットが正しくない場合、登録に失敗すること" do
-      company = build(:company, postal_code: "0000000")
-      company.valid?
-      expect(company.errors[:postal_code]).to include("は不正な値です")
-    end
-
+    
+    
     it "addressがない場合は登録に失敗すること" do
       company = build(:company, address: nil)
       company.valid?
       expect(company.errors[:address]).to include("を入力してください")
     end
+    
+    context "フォーマット" do
+      it "telのフォーマットが正しくない場合、登録に失敗すること" do
+        company = build(:company, tel: "1000000000")
+        company.valid?
+        expect(company.errors[:tel]).to include("は不正な値です")
+      end
+      
+      it "faxのフォーマットが正しくない場合、登録に失敗すること" do
+        company = build(:company, fax: "1000000000")
+        company.valid?
+        expect(company.errors[:fax]).to include("は不正な値です")
+      end
 
+      it "postal_codeのフォーマットが正しくない場合、登録に失敗すること" do
+        company = build(:company, postal_code: "0000000")
+        company.valid?
+        expect(company.errors[:postal_code]).to include("は不正な値です")
+      end
+    end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -19,47 +19,49 @@ RSpec.describe User, type: :model do
       expect(user.errors[:email]).to include("を入力してください")
     end
 
-    it "emailのフォーマットが正しくない場合、登録に失敗すること" do
-      user = build(:user, email: "exampletestcom")
-      user.valid?
-      expect(user.errors[:email]).to include "は不正な値です"
-end
-
+    
     it "passwordがなければ登録に失敗すること" do
       user = build(:user, password: nil)
       user.valid?
       expect(user.errors[:password]).to include("を入力してください")
     end
-
+    
     it "passwordが存在してもpassword_confirmationがない場合登録に失敗すること" do
       user = build(:user, password_confirmation: "")
       user.valid?
       expect(user.errors[:password_confirmation]).to include("とPasswordの入力が一致しません")
     end
-
+    
     it "重複したemailが存在する場合登録に失敗すること" do
       user = create(:user)
       another_user = build(:user, email: user.email)
       another_user.valid?
       expect(another_user.errors[:email]).to include("はすでに存在します")
     end
-
+    
     it "passwordが6文字以上かつ半角英数字を両方含む場合であれば登録できること" do
       user = build(:user, password: "ryota1", password_confirmation: "ryota1")
       expect(user).to be_valid 
     end
-
+    
     it "passwordが６文字以下の場合登録に失敗すること" do
       user = build(:user, password: "ryo11")
       user.valid?
       expect(user.errors[:password]).to include("は6文字以上で入力してください")
     end
-
+    
     it "passwordが半角英数字以外を含む場合登録に失敗すること" do
       user = build(:user, password: "ryotaか")
       user.valid?
       expect(user.errors[:password]).to include("は不正な値です")
     end
-
+    
+    context "フォーマット" do
+      it "emailのフォーマットが正しくない場合、登録に失敗すること" do
+        user = build(:user, email: "exampletestcom")
+        user.valid?
+        expect(user.errors[:email]).to include "は不正な値です"
+      end
+    end
   end
 end
