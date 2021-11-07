3fb370b3c403ff5099c72bc2371a75f35fa3ce59
ryota1119 <ibaryo.ngok.sxs@gmail.com>
2021-11-07 15:12:28 +0900

    Create user model test

---
+++ spec/factories/user.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :user do
+    name                    {"篠原亮太"}
+    email                   {"ryota@ryota.com"}
+    password                {"ryota1119"}
+    password_confirmation   {"ryota1119"}
+  end
+end
