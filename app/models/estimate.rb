8538c677c96d75ddb52d44306a5056a6d7f66e04
ryota1119 <ibaryo.ngok.sxs@gmail.com>
2021-11-10 20:59:53 +0900

    Edit new html estimates

--- app/models/estimate.rb
+++ app/models/estimate.rb
@@ -3,4 +3,11 @@ class Estimate < ApplicationRecord
   belongs_to :user
   belongs_to :customer
   belongs_to :contact_person
+  validate :date_before_start
+
+  def date_before_start
+    return if effective.blank?
+    errors.add(:effective, "は今日以降のものを選択してください") if effective < Date.today
+  end
+
 end
