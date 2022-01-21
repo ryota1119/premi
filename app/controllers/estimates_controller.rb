3a7d0d23912370e682c64e80df6d02d7261ee3f2
ryota1119 <ibaryo.ngok.sxs@gmail.com>
2022-01-21 18:04:58 +0900

    Update estimate controller & estimate new page

--- app/controllers/estimates_controller.rb
+++ app/controllers/estimates_controller.rb
@@ -1,41 +1,38 @@
 class EstimatesController < ApplicationController
   before_action :authenticate_user!
-
   def index
     @user = current_user
     @customers = @user.customers
     @estimates = @user.estimates
     
   end
-
   def show
     @estimate = Estimate.find(params[:id])
     @customer = @estimate.customer
     @contact_person = @estimate.contact_person
     @user = current_user
   end
-
   def new
     @estimate = EstimateForm.new
     @user = current_user
-    # @item = ItemCollection.new
   end
-
   def create
     @estimate = EstimateForm.new(estimate_params)
+    binding.pry
     if @estimate.save
       redirect_to estimates_path
     else
-      flash.now[:alert] = "登録に失敗しました"
       render :new, status: :unprocessable_entity
     end
   end
-
   def edit
+    # @estimate = EstimateForm.find()
+    # @user = current_user
   end
-
+  def update
+    # @estimate = EstimateForm.new(estimate_params, @estimate)
+  end
   private
-
   def estimate_params
     params.require(:estimate_form).permit(
       :company_name, :department, :position, :name, :effective, :item_name, :volume, :price, :note
